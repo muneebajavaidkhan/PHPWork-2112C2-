0f398c77cee214da5f173ce59c84b3c6a735df85
Muneebakhan <muneebakhan430@gmail.com>
2022-11-22 13:02:39 +0500

    Updated work 22-11-22

---
+++ ImageUploadingWork/connection.php
@@ -0,0 +1,12 @@
+   <?php
+   $con = mysqli_connect('localhost', 'root', '', 'batch2112c2'); //true//false
+   if (!$con) {
+       echo 'Connection Failed';
+   }
+
+   //    else {
+   //        echo 'Connection Failed';
+   //    }
+
+
+?>
