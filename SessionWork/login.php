d83639ab59b2f139a7e486bdc9321a73243ceffd
Muneebakhan <muneebakhan430@gmail.com>
2022-11-29 11:52:20 +0500

    Updated work

---
+++ SessionWork/login.php
@@ -0,0 +1,72 @@
+
+<?php include 'header.php'; ?>
+<?php include 'connection.php'; ?>
+
+
+
+<div class = 'container mt-5'>
+
+        
+                <H1 class =  "text-center">LOGIN FORM</H1>
+                <form action="login.php" method = "post">
+                    <div class="row mt-5">
+
+                        
+
+                        <div class="col-sm-12 col-lg-6 mt-5">
+
+                            
+
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="email" class="form-control" id="email" placeholder="Enter Your Email" name="email" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="Pass">Password:</label>
+                                <input type="password" class="form-control" id="Pass" placeholder="Enter Your Password" name="Pass" required>
+                            </div>
+                           
+                            <div class="form-group">
+                                <input type="submit" value = "Login Account" class = "btn btn-primary" name = "btn">
+                            </div>
+                            <p class = "text-center">Don't Have an Account <a href="register.php">Signup</a></p>
+                        </div>
+
+
+                        <div class="col-sm-12 col-lg-6 ">
+                            <img src="images/img2.jpg" alt="dummyImage" style = "width:350px;height:280px;margin-left:95px">
+                        </div>
+
+                    </div>
+                  
+                </form>
+
+
+         
+
+</div>
+
+
+
+
+<?php if (isset($_POST['btn'])) {
+    $Email = $_POST['email'];
+    $Pass = $_POST['Pass'];
+    $EmailSearch = "select * from register where Email = '$Email'"; //abc@gmail.com
+    $query = mysqli_query($con, $EmailSearch);
+    $EmailCount = mysqli_num_rows($query);
+    //1
+    if ($EmailCount) {
+        $res = mysqli_fetch_assoc($query);
+        $db_Pass = $res['Password']; //16 digit
+        $Pass_decode = password_verify($Pass, $db_Pass);
+        //123
+        if ($Pass_decode) {
+            echo "<script>alert('Login Succesfull');window.location.href = 'Main.php';</script>";
+        } else {
+            echo "<script>alert('Login Failed')</script>";
+        }
+    } else {
+        echo "<script>alert('Invalid Email!!')</script>";
+    }
+} ?>
---
+++ SessionWork/register.php
@@ -0,0 +1,93 @@
+<?php include 'header.php'; ?>
+<?php include 'connection.php'; ?>
+
+
+
+
+    
+<div class = 'container mt-5'>
+
+        
+                <H1 class =  "text-center">REGISTRATION FORM</H1>
+                <form action="register.php" method = "post">
+                    <div class="row mt-5">
+
+                        <div class="col-sm-12 col-lg-6 ">
+                            <img src="images/img1.jpg" alt="dummyImage" style = "width:350px;height:280px;margin-left:95px">
+                        </div>
+
+                        <div class="col-sm-12 col-lg-6 mt-5">
+
+                            <div class="form-group">
+                                <label for="name">Name:</label>
+                                <input type="text" class="form-control" id="name" placeholder="Enter Your name" name="name" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="phone">Phone Number:</label>
+                                <input type="text" class="form-control" id="phone" placeholder="Enter Your Phone Number" name="Phone" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="email" class="form-control" id="email" placeholder="Enter Your Email" name="email" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="Pass">Password:</label>
+                                <input type="password" class="form-control" id="Pass" placeholder="Enter Your Password" name="Pass" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="Confpass">Confirm Password:</label>
+                                <input type="password" class="form-control" id="Confpass" placeholder="Enter Your Confirm Password" name="ConfPass" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" value = "Create Account" class = "btn btn-primary" name = "btn">
+                            </div>
+                            <p class = "text-center">Already Have an Account <a href="login.php">Login</a></p>
+                        </div>
+
+                    </div>
+                  
+                </form>
+
+
+         
+
+</div>
+
+<?php if (isset($_POST['btn'])) {
+    $Name = $_POST['name'];
+    $Email = $_POST['email']; //abc@gmail.com
+    $Phone = $_POST['Phone'];
+    $Pass = $_POST['Pass']; //123
+    $CPass = $_POST['ConfPass']; //123
+    $Password = password_hash($Pass, PASSWORD_BCRYPT);
+    $CPassword = password_hash($CPass, PASSWORD_BCRYPT);
+    $Emailquery = "select * from register where Email = '$Email'"; //abc@gmail.com = xyz@gmail.com
+    $res = mysqli_query($con, $Emailquery);
+    if (mysqli_num_rows($res) > 0) {
+        //0
+        echo "<script>alert('Email Already Exist!!')</script>";
+    } else {
+        if ($Pass === $CPass) {
+            $Insquery = "insert into register (Name,PhoneNum,Password,ConfPass,Email) values ('$Name','$Phone','$Password','$CPassword','$Email')";
+            $rs = mysqli_query($con, $Insquery);
+            if ($rs) {
+                echo "<script>alert('Data Inserted')</script>";
+            } else {
+                echo "<script>alert('Data not Inserted')</script>";
+            }
+        } else {
+            echo "<script>alert('Password not match!!')</script>";
+        }
+    }
+} ?>
+
+
+
+
+
+
+
+
+
+
